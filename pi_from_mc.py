c35a3af22b4dc8da7ddcabc12713f7190860e5b2
Xiaoliang Zhang <zhxlhdd2008@gmail.com>
2015-10-18 05:51:02 +0800

    Create pi_from_mc.py

---
+++ pi_from_mc.py
@@ -0,0 +1,22 @@
+import random
+
+ofile = 'pi.dat'
+fo = file(ofile,'w')
+
+num_pi    = 0.0
+
+nparticle = 10000000
+i_cicle   = 0
+i_square  = 0
+for i in range(nparticle):
+  i_square+=1
+  random_number1 = random.random()
+  random_number2 = random.random()
+  if random_number1**2+random_number2**2<1.0:
+    i_cicle+=1
+  num_pi = float(i_cicle)/float(i_square)*4.0
+  print i_square, num_pi
+  fo.write('%8i ' %i_square)
+  fo.write('%25.15g ' %num_pi)
+  fo.write('\n')
+fo.close()
